79bf373edc06c10c280aedc823688cec576dfae3
andrea-croci-its <andrea.croci.studio@fitstic-edu.com>
2025-05-07 23:20:43 +0200

    Merge with branch: AndreaCroci
    Added:
    - navbar
    - javascript hashing functions
    - javascript haveibeenpwnd check
    - password generator
    - passphrase generator
    - master-domain generator

---
+++ display.js
@@ -0,0 +1,73 @@
+let __DISPLAY_COLOR = {
+  password: 'blue',
+  error: 'red',
+}
+
+// Tailwind
+// "bg-red-100 bg-yellow-100 bg-green-100 bg-blue-100"
+// "bg-red-600 bg-yellow-600 bg-green-600 bg-blue-600"
+// "text-red-600 text-yellow-600 text-green-600 text-blue-600"
+// "border-red-600 border-yellow-600 border-green-600 border-blue-600"
+// "hover:bg-red-100 hover:bg-yellow-100 hover:bg-green-100 hover:bg-blue-100"
+
+function setPassword(count, color, password) {
+  const passwordText = document.getElementById('passwordText')
+  const passwordIcon = document.getElementById('passwordIcon')
+  const passwordBox = document.getElementById('passwordBox')
+  // Remove previous border color
+  passwordBox.classList.remove(`border-${__DISPLAY_COLOR.password}-600`)
+  passwordIcon.classList.remove(`bg-${__DISPLAY_COLOR.password}-600`)
+  passwordText.classList.remove(`border-${__DISPLAY_COLOR.password}-600`)
+  passwordText.classList.remove(`hover:bg-${__DISPLAY_COLOR.password}-100`)
+  __DISPLAY_COLOR.password = color
+
+  // Sets up the display HTML
+  if (password == 'Generated Password') {
+    passwordText.classList.remove('text-gray-900')
+    passwordText.classList.add('text-gray-300')
+  } else {
+    passwordText.classList.remove('text-gray-300')
+    passwordText.classList.add('text-gray-900')
+    passwordText.classList.add(`hover:bg-${color}-100`)
+  }
+  if (count == 0 || count == null) {
+    count = '<img class="w-4 h-4" src="icon/check.svg"/>'
+  }
+
+  // Applies the changes to the page elements
+  passwordIcon.innerHTML = count
+  passwordText.innerText = password
+  passwordBox.classList.add(`border-${color}-600`)
+  passwordIcon.classList.add(`bg-${color}-600`)
+  passwordText.classList.add(`border-${color}-600`)
+}
+
+function setError(icon = '', color = '', title = '', description = []) {
+  const errorDisplay = document.getElementById('errorDisplay')
+  const errorIcon = document.getElementById('errorIcon')
+  const errorDescription = document.getElementById('errorDescription')
+
+  if (color == null || color == '') {
+    errorDisplay.classList.remove('flex')
+    errorDisplay.classList.add('hidden')
+    return
+  }
+  // Remove previous background and text color.
+  errorDisplay.classList.remove(`bg-${__DISPLAY_COLOR.error}-100`)
+  errorDisplay.classList.remove(`text-${__DISPLAY_COLOR.error}-600`)
+  __DISPLAY_COLOR.error = color
+
+  // Build the display information
+  display = `<p>${title}</p>`
+  for (text of description) {
+    display += `<p class="pl-2 text-sm">${text}</p>`
+  }
+
+  // Applies the changes to the page elements.
+  errorIcon.src = icon
+  errorDescription.innerHTML = display
+  errorDisplay.classList.add(`bg-${color}-100`)
+  errorDisplay.classList.add(`text-${color}-600`)
+  errorDisplay.classList.add('flex')
+  errorDisplay.classList.remove('hidden')
+}
---
+++ script.js
@@ -0,0 +1,132 @@
+const charset = {
+  lower: 'abcdefghijklmnopqrstuvwxyz',
+  upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  number: '0123456789',
+  symbol: '!#$%&?@()*+-;<=>{[]}/,_',
+}
+
+function base85(hexstring) {
+  const B85 = charset.lower + charset.upper + charset.number + charset.symbol
+  let result = ''
+
+  for (let i = 0; i < hexstring.length; i += 8) {
+    // Selects the next 4 bytes and converts them to int32
+    bytes = hexstring.substring(i, i + 8).padEnd(8, '0')
+    bytes = parseInt(bytes, 16)
+    // Converts the int32 to a base85 string
+    while (bytes > 0) {
+      result += B85[bytes % 85]
+      bytes = Math.floor(bytes / 85)
+    }
+  }
+
+  return result
+}
+
+async function hash(algorithm, string) {
+  const buffer = new TextEncoder().encode(string)
+
+  // Hashes the given string with the given algorithm.
+  const digest = await crypto.subtle.digest(algorithm, buffer)
+  const bytes = Array.from(new Uint8Array(digest))
+
+  // Maps each byte to a string representation in hex.
+  return bytes.map((b) => b.toString(16).padStart(2, '0')).join('')
+}
+
+async function checkPwned(password) {
+  const sha1 = await hash('SHA-1', password)
+
+  const prefix = sha1.substring(0, 5).toUpperCase()
+  const suffix = sha1.substring(5).toUpperCase()
+
+  // Query to the havibeenpwned API.
+  const response = await fetch(`https://api.pwnedpasswords.com/range/${prefix}`)
+  const text = await response.text()
+
+  // Split the results in suffix:count.
+  const hashes = text.split('\n').map((line) => line.split(':'))
+
+  for (const [hashSuffix, count] of hashes) {
+    if (hashSuffix.trim() === suffix) {
+      // Return number of times it appeared.
+      return parseInt(count, 10)
+    }
+  }
+  // Not found.
+  return 0
+}
+
+function checkEntropy_charset(password) {
+  if (!password.length) return 0
+
+  // Count the total sets of characters used.
+  const count = {
+    lower: false,
+    upper: false,
+    number: false,
+    symbol: false,
+  }
+
+  for (const char of password) {
+    if (charset['lower'].includes(char)) {
+      count['lower'] = true
+    } else if (charset['upper'].includes(char)) {
+      count['upper'] = true
+    } else if (charset['number'].includes(char)) {
+      count['number'] = true
+    } else {
+      count['symbol'] = true
+    }
+  }
+
+  // Calculates N.
+  let N = 1
+  N += 26 * count['lower']
+  N += 26 * count['upper']
+  N += 10 * count['number']
+  N += 30 * count['symbol']
+
+  // Calculates the total entropy.
+  return password.length * Math.log2(N)
+}
+
+function checkEntropy_frequency(password) {
+  if (!password.length) return 0
+
+  // Count the frequency of each character.
+  const frequencies = {}
+  for (const char of password) {
+    frequencies[char] = (frequencies[char] || 0) + 1
+  }
+
+  // Measure the length of the password.
+  const len = password.length
+  let entropy = 0
+
+  for (const char in frequencies) {
+    const frequency = frequencies[char] / len
+    entropy -= frequency * Math.log2(frequency)
+  }
+
+  // Total entropy (in bits).
+  return entropy * len
+}
+
+function randInt(min, max) {
+  return min + (Math.floor(Math.random() * 1e9) % (max - min))
+}
+
+function randChoice(collection) {
+  index = randInt(0, collection.length)
+  return collection[index]
+}
+
+function copyToClipboard(text) {
+  navigator.permissions.query({ name: 'clipboard-write' }).then((result) => {
+    if (result.state === 'granted' || result.state === 'prompt') {
+      /* write to the clipboard now */
+      navigator.clipboard.writeText(text)
+    }
+  })
+}
